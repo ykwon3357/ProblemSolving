28ee98a8f362389abd506738fc272d96434f7420
ykwon3357 <ykwon3357@naver.com>
2021-05-17 16:28:31 +0900

    update 2609

--- prob/2609_최대공약수와 최소공배수.py
+++ prob/2609_최대공약수와 최소공배수.py
@@ -1,19 +1,4 @@
 # -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.11.2
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
 # +
 a, b = map(int, input().split())
 oa, ob = a, b
